d60725fe0e00fa1475e771b38f890db9dcc55bdd
Angelina Cortes <cortesporcz@gmail.com>
2024-04-13 17:52:28 -0500

    webpack todos were to Add and configure workbox plugins for a service worker and manifest file and Added CSS loaders and babel to webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -6,8 +6,7 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
-module.exports = () => {
-  return {
+module.exports = {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
@@ -18,13 +17,49 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'J.A.T.E.',
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+      new WebpackPwaManifest({
+        name: 'J.A.T.E.',
+        short_name: 'J.A.T.E',
+        description: 'Just Another Text Editor',
+        background_color: '#ffffff',
+        crossorigin: 'use-credentials',
+        inject: true,
+        fingerprints: false,
+        start_url: '/',
+        publicPath: '/',
+        icons: [{
+          src: path.resolve(__dirname, 'src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        }]
+      }),
     ],
 
     module: {
       rules: [
-        
+        {test: /\.css$/i,
+          use: ['style-loader', 'css-loader']},
+        {test: /\.m?js$/,
+      exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/presest-env'],
+        plugins: [
+          '@babel/plugin-transform-object-rest-spread',
+          '@babel/transform-runtime'
+        ]
+      }
+    }}
       ],
     },
   };
-};
