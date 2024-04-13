2789f68ab8b65ea7d4a2db94b0cdca14f9f34e9f
Angelina Cortes <cortesporcz@gmail.com>
2024-04-13 15:18:32 -0500

    adding event handlers via starter code

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -18,7 +18,7 @@ butInstall.addEventListener('click', async () => {
     butInstall.classList.toggle('hidden', true);
 });
 
-// TODO: Add an handler for the `appinstalled` event
+// DONE-TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
     window.deferredPrompt = null;
 });
